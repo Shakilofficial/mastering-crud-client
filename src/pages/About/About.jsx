e691c6bc8e4c3c155316f02e8fe50a8cfe952e78
ShakilOfficials <mrshakilhossainofficial@gmail.com>
2024-07-08 13:33:16 +0600

    update about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,26 @@
 const About = () => {
   return (
-    <div>
-      <h1>This is the About component</h1>
+    <div className="sm:flex items-center">
+      <div className="sm:w-1/2 p-10">
+        <div className="image object-center text-center">
+          <img src="https://i.imgur.com/WbQnbas.png" />
+        </div>
+      </div>
+      <div className="sm:w-1/2 p-5">
+        <div className="text">
+          <span className="text-gray-500 border-b-2 border-green-600 uppercase">
+            About us
+          </span>
+          <h2 className="my-4 font-bold text-3xl  sm:text-4xl ">
+            About <span className="text-green-600">Our Company</span>
+          </h2>
+          <p className="text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid,
+            commodi doloremque, fugiat illum magni minus nisi nulla numquam
+            obcaecati placeat quia, repellat tempore voluptatum.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
