8c075243644e715ceca001b401526c44e75534c3
ShakilOfficials <mrshakilhossainofficial@gmail.com>
2024-07-13 12:13:16 +0600

    add pagination on service page

---
+++ src/components/ui/Loading.jsx
@@ -0,0 +1,12 @@
+const Loading = () => {
+  return (
+    <div className="text-green-500 flex justify-center items-center w-3/4 mx-auto">
+      <span className="loading loading-bars loading-xs"></span>
+      <span className="loading loading-bars loading-sm"></span>
+      <span className="loading loading-bars loading-md"></span>
+      <span className="loading loading-bars loading-lg"></span>
+    </div>
+  );
+};
+
+export default Loading;
--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -2,11 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import ServiceCard from "../../components/ServiceCard";
 import Container from "../../components/ui/Container";
+import Error from "../../components/ui/Error";
 import Header from "../../components/ui/Header";
 import useAxios from "../../hooks/useAxios";
 import { capitalizeWords } from "../../utils/capitalizeWords";
+import Loading from "../../components/ui/Loading";
 
 const categories = [
+  "All",
   "Hosting",
   "Marketing",
   "Design",
@@ -26,10 +29,13 @@ const Services = () => {
 
   const [price, setPrice] = useState("");
   const [category, setCategory] = useState("");
+  const [page, setPage] = useState(1);
+
+  const limit = 9;
 
   const getServices = async () => {
     const res = await axios.get(
-      `/services?sortField=price&sortOrder=${price}&category=${category}`
+      `/services?sortField=price&sortOrder=${price}&category=${category}&page=${page}&limit=${limit}`
     );
     return res;
   };
@@ -38,49 +44,34 @@ const Services = () => {
     isError,
     isLoading,
   } = useQuery({
-    queryKey: ["service", price, category],
+    queryKey: ["service", price, category, page],
     queryFn: getServices,
   });
 
   if (isLoading) {
-    return (
-      <div className="text-green-500 flex justify-center items-center w-3/4 mx-auto">
-        <span className="loading loading-bars loading-xs"></span>
-        <span className="loading loading-bars loading-sm"></span>
-        <span className="loading loading-bars loading-md"></span>
-        <span className="loading loading-bars loading-lg"></span>
-      </div>
-    );
+    return <Loading />;
   }
+  const handlePrevious = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+  const handleNext = () => {
+    if (page < totalPage) {
+      setPage(page + 1);
+    }
+  };
+  const totalPage = Math.ceil(services?.data?.total / limit);
 
   if (isError) {
-    return (
-      <div className="text-green-500 flex justify-center items-center w-3/4 mx-auto mt-24">
-        <div role="alert" className="alert alert-error">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 shrink-0 stroke-current"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-          <span>Error! Task failed successfully.</span>
-        </div>
-      </div>
-    );
+    return <Error />;
   }
   return (
     <div>
       <Container className="mb-64">
         <Header
           title="Services"
-          description="We Provide Different Cleaning Services"
+          description="We Provide Different Types of Services"
         />
         <div className="flex justify-center space-x-4 p-4 bg-green-300 rounded-lg shadow-lg mb-10">
           <div>
@@ -128,6 +119,62 @@ const Services = () => {
           {services?.data?.result.map((item) => (
             <ServiceCard key={item._id} service={item} />
           ))}
+        </div>
+        <div className="flex justify-center items-center my-12">
+          <div className="join border-2 border-green-500">
+            <button
+              onClick={handlePrevious}
+              className="join-item btn btn-ghost"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 19.5 8.25 12l7.5-7.5"
+                />
+              </svg>
+            </button>
+            {[...Array(totalPage).fill(0)].map((item, index) => {
+              const pageNumber = index + 1;
+              return (
+                <button
+                  key={pageNumber}
+                  onClick={() => setPage(pageNumber)}
+                  className={`${
+                    pageNumber === page
+                      ? "join-item btn btn-primary"
+                      : "join-item btn btn-ghost"
+                  }`}
+                >
+                  {pageNumber}
+                </button>
+              );
+            })}
+
+            <button onClick={handleNext} className="join-item btn btn-ghost">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
       </Container>
     </div>
